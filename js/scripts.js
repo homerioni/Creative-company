f851c19b0c7c15d29f03ca9cb88d3cb9e165a42e
homerioni <homerion13@gmail.com>
2021-12-02 22:00:49 +0300

    add slider-slick, jquery and html section clients

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('.multiple-items').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 2
+    });
+
+});
